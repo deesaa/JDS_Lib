d5dfe04f015edd1b7290419fd69b58387e28a013
jadesa <jadeesaa@gmail.com>
2021-06-04 14:26:59 +0300

    MonoBehaviour Bind extension BindHandler

---
+++ JDS/BindBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JDS
+{
+    public class BindBehaviour<T> : MonoBehaviour where T : Enum
+    {
+        private List<BindHandler<T>> _bindHandlers = new List<BindHandler<T>>();
+        
+        private void OnDestroy()
+        {
+            foreach (var bindHandler in _bindHandlers)
+            {
+                bindHandler.Destroy();
+            }
+            AfterDestroy();
+        }
+
+        protected void Bind(T valueType, Action action)
+        {
+            _bindHandlers.Add(GRC<T>.Bind(valueType, action));
+        }
+
+        protected virtual void AfterDestroy() { }
+    }
+}
