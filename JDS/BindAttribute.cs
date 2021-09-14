42ce945ed7bdda8d9a28d8b0f296835579930356
jadesa <jadeesaa@gmail.com>
2021-09-14 11:52:37 +0300

    cleanup

--- JDS/BindAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace JDS
-{
-    public class BindAttribute : Attribute
-    {
-        
-    }
-}
--- JDS/EcsExt.cs
+++ JDS/EcsExt.cs
@@ -5,23 +5,6 @@ namespace Client.Systems
 {
     public static class EcsExt
     {
-        /*public static bool Contains<T>(this EcsFilter<T> filter, object value) where T : struct
-        {
-            if (filter.IsEmpty())
-                return false;
-            
-            bool contains = false;
-            foreach (int index in filter)
-            {
-                if (filter.Get1(index).Equals(value))
-                {
-                    contains = true;
-                    break;
-                }
-            }
-            return contains;
-        }*/
-        
         public static EcsGameState Add<T>(this GSM<T> gsm, T name, EcsGameState ecsGameState, EcsWorld world)
         {
             ecsGameState.SetWorld(world);
